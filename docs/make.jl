f0018845dc5e1ebcb623220a4d24a08d77a23203
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-07-26 22:21:46 -0400

    Formatting pass

--- docs/make.jl
+++ docs/make.jl
@@ -2,9 +2,9 @@ using Documenter
 using SymEngine
 using TinyEKFGen
 
-makedocs(sitename = "TinyEKFGen", format = Documenter.HTML(), modules = [TinyEKFGen])
+makedocs(sitename="TinyEKFGen", format=Documenter.HTML(), modules=[TinyEKFGen])
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(repo = "github.com/ThatcherC/TinyEKFGen.jl.git")
+deploydocs(repo="github.com/ThatcherC/TinyEKFGen.jl.git")
